add312c6533b3c837ba19bda26475266d3119597
jingtao <jingtao@tju.edu.cn>
2026-03-01 12:20:03 +0800

    patch: add data pub zmq manager

---
+++ Src/Src/Zmq/DataPubZmqManager.h
@@ -0,0 +1,28 @@
+/**************************************************************************
+
+           Copyright(C), tao.jing All rights reserved
+
+ **************************************************************************
+   File   : DataPubZmqManager.h
+   Author : tao.jing
+   Date   : 2026.03.01
+   Brief  :
+**************************************************************************/
+#ifndef FIREAPP_DATAPUBZMQMANAGER_H
+#define FIREAPP_DATAPUBZMQMANAGER_H
+
+#include <TSingleton.h>
+
+
+namespace TF {
+
+    class DataPubZmqManager : public TBase::TSingleton<DataPubZmqManager>
+    {
+
+
+    };
+
+};
+
+
+#endif //FIREAPP_DATAPUBZMQMANAGER_H
